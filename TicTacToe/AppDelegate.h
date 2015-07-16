6fe812ef48c27bc98ad6508d263cd3f4da1d7216
Sean Allen <allen.sean.t@gmail.com>
2015-07-16 13:09:58 -0700

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Sean's Macboo Pro on 7/16/15.
+//  Copyright (c) 2015 seanallen.co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Sean's Macboo Pro on 7/16/15.
+//  Copyright (c) 2015 seanallen.co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
