1a68c16fe366bb3e7a0587e5124843ec92ea9902
Sean Allen <allen.sean.t@gmail.com>
2015-07-16 13:14:46 -0700

    board layout

---
+++ TicTacToe/GameViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  TicTacToe
+//
+//  Created by Sean's Macboo Pro on 7/16/15.
+//  Copyright (c) 2015 seanallen.co. All rights reserved.
+//
+
+#import "GameViewController.h"
+
+@interface GameViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *topLeftLabel;
+@property (weak, nonatomic) IBOutlet UILabel *topMiddleLable;
+@property (weak, nonatomic) IBOutlet UILabel *topRightLabel;
+@property (weak, nonatomic) IBOutlet UILabel *middleLeftLabel;
+@property (weak, nonatomic) IBOutlet UILabel *middleMiddleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *middleRightLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bottomLeftLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bottomMiddleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bottomRightLabel;
+
+@end
+
+@implementation GameViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+
+@end
