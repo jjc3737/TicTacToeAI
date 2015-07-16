b073348e363017ed0b491a505585bd747c0dbcb9
Sean Allen <allen.sean.t@gmail.com>
2015-07-16 13:51:58 -0700

    big if statement done

--- TicTacToe/GameViewController.m
+++ TicTacToe/GameViewController.m
@@ -10,7 +10,7 @@
 
 @interface GameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *topLeftLabel;
-@property (weak, nonatomic) IBOutlet UILabel *topMiddleLable;
+@property (weak, nonatomic) IBOutlet UILabel *topMiddleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *topRightLabel;
 @property (weak, nonatomic) IBOutlet UILabel *middleLeftLabel;
 @property (weak, nonatomic) IBOutlet UILabel *middleMiddleLabel;
@@ -18,6 +18,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *bottomLeftLabel;
 @property (weak, nonatomic) IBOutlet UILabel *bottomMiddleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *bottomRightLabel;
+@property (weak, nonatomic) IBOutlet UILabel *playerLabel;
 
 @end
 
@@ -25,9 +26,51 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (NSString *) findLabelUsingPoint:(CGPoint) userTappedCGPoint {
 
+    NSString *labelName = nil;
+
+    if (CGRectContainsPoint(self.topLeftLabel.frame, userTappedCGPoint)) {
+
+        labelName = @"topLeftLabel";
+    } else if (CGRectContainsPoint(self.topMiddleLabel.frame, userTappedCGPoint)) {
+
+        labelName = @"topMiddleLabel";
+    } else if (CGRectContainsPoint(self.topRightLabel.frame, userTappedCGPoint)) {
+
+        labelName = @"topRightLabel";
+    } else if (CGRectContainsPoint(self.middleLeftLabel.frame, userTappedCGPoint)) {
+
+        labelName = @"middleLeftLabel";
+    } else if (CGRectContainsPoint(self.middleMiddleLabel.frame, userTappedCGPoint)) {
+
+        labelName = @"middleMiddleLabel";
+    } else if (CGRectContainsPoint(self.middleRightLabel.frame, userTappedCGPoint)) {
+
+        labelName = @"middleRightLabel";
+    } else if (CGRectContainsPoint(self.bottomLeftLabel.frame, userTappedCGPoint)) {
+
+        labelName = @"bottomLeftLabel";
+    } else if (CGRectContainsPoint(self.bottomMiddleLabel.frame, userTappedCGPoint)) {
+
+        labelName = @"bottomMiddleLabel";
+    } else if (CGRectContainsPoint(self.bottomRightLabel.frame, userTappedCGPoint)) {
+
+        labelName = @"bottomRightLabel";
+    }
+
+    return labelName;
+
+}
+
+- (IBAction)onLabelTapped:(UITapGestureRecognizer *)sender {
+    CGPoint tappedPoint = [sender locationInView:self.view];
+    NSString *labelPressed = [self findLabelUsingPoint:tappedPoint];
+
+}
 
 @end
