19db62df5320d4fdc02b35f8a093cbb5b56c7586
jjc3737 <Jane@Jaehees-MacBook-Pro.local>
2015-07-16 14:33:26 -0700

    Finished Task 3

--- TicTacToe/GameViewController.m
+++ TicTacToe/GameViewController.m
@@ -19,6 +19,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *bottomMiddleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *bottomRightLabel;
 @property (weak, nonatomic) IBOutlet UILabel *playerLabel;
+@property NSString *currentPlayer;
 
 @end
 
@@ -26,8 +27,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.currentPlayer = @"X";
 
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (NSString *) findLabelUsingPoint:(CGPoint) userTappedCGPoint {
@@ -61,6 +62,8 @@
     } else if (CGRectContainsPoint(self.bottomRightLabel.frame, userTappedCGPoint)) {
 
         labelName = @"bottomRightLabel";
+    } else {
+        labelName = nil;
     }
 
     return labelName;
@@ -71,6 +74,92 @@
     CGPoint tappedPoint = [sender locationInView:self.view];
     NSString *labelPressed = [self findLabelUsingPoint:tappedPoint];
 
-}
 
+    if ([self.currentPlayer isEqualToString:@"X"]) {
+        if ([labelPressed isEqualToString:@"topLeftLabel"]) {
+            self.topLeftLabel.text = @"X";
+            self.topLeftLabel.textColor = [UIColor blueColor];
+
+        } else if ([labelPressed isEqualToString:@"topMiddleLabel"]) {
+            self.topMiddleLabel.text = @"X";
+            self.topMiddleLabel.textColor = [UIColor blueColor];
+
+        } else if ([labelPressed isEqualToString:@"topRightLabel"]) {
+            self.topRightLabel.text = @"X";
+            self.topRightLabel.textColor = [UIColor blueColor];
+
+        } else if ([labelPressed isEqualToString:@"middleLeftLabel"]) {
+            self.middleLeftLabel.text = @"X";
+            self.middleLeftLabel.textColor = [UIColor blueColor];
+
+        } else if ([labelPressed isEqualToString:@"middleMiddleLabel"]) {
+            self.middleMiddleLabel.text = @"X";
+            self.middleMiddleLabel.textColor = [UIColor blueColor];
+
+        } else if ([labelPressed isEqualToString:@"middleRightLabel"]) {
+            self.middleRightLabel.text = @"X";
+            self.middleRightLabel.textColor = [UIColor blueColor];
+
+        } else if ([labelPressed isEqualToString:@"bottomLeftLabel"]) {
+            self.bottomLeftLabel.text = @"X";
+            self.bottomLeftLabel.textColor = [UIColor blueColor];
+
+        } else if ([labelPressed isEqualToString:@"bottomMiddleLabel"]) {
+            self.bottomMiddleLabel.text = @"X";
+            self.bottomMiddleLabel.textColor = [UIColor blueColor];
+
+        } else if ([labelPressed isEqualToString:@"bottomRightLabel"]) {
+            self.bottomRightLabel.text = @"X";
+            self.bottomRightLabel.textColor = [UIColor blueColor];
+        }
+
+        self.currentPlayer = @"O";
+
+    } else if ([self.currentPlayer isEqualToString:@"O"]) {
+
+        if ([labelPressed isEqualToString:@"topLeftLabel"]) {
+            self.topLeftLabel.text = @"O";
+            self.topLeftLabel.textColor = [UIColor redColor];
+
+        } else if ([labelPressed isEqualToString:@"topMiddleLabel"]) {
+            self.topMiddleLabel.text = @"O";
+            self.topMiddleLabel.textColor = [UIColor redColor];
+
+        } else if ([labelPressed isEqualToString:@"topRightLabel"]) {
+            self.topRightLabel.text = @"O";
+            self.topRightLabel.textColor = [UIColor redColor];
+
+        } else if ([labelPressed isEqualToString:@"middleLeftLabel"]) {
+            self.middleLeftLabel.text = @"O";
+            self.middleLeftLabel.textColor = [UIColor redColor];
+
+        } else if ([labelPressed isEqualToString:@"middleMiddleLabel"]) {
+            self.middleMiddleLabel.text = @"O";
+            self.middleMiddleLabel.textColor = [UIColor redColor];
+
+        } else if ([labelPressed isEqualToString:@"middleRightLabel"]) {
+            self.middleRightLabel.text = @"O";
+            self.middleRightLabel.textColor = [UIColor redColor];
+
+        } else if ([labelPressed isEqualToString:@"bottomLeftLabel"]) {
+            self.bottomLeftLabel.text = @"O";
+            self.bottomLeftLabel.textColor = [UIColor redColor];
+
+        } else if ([labelPressed isEqualToString:@"bottomMiddleLabel"]) {
+            self.bottomMiddleLabel.text = @"O";
+            self.bottomMiddleLabel.textColor = [UIColor redColor];
+                
+        } else if ([labelPressed isEqualToString:@"bottomRightLabel"]) {
+            self.bottomRightLabel.text = @"O";
+            self.bottomRightLabel.textColor = [UIColor redColor];
+            }
+        self.currentPlayer = @"X";
+    }
+
+    if ([self.currentPlayer isEqualToString:@"X"]) {
+        self.playerLabel.text = @"O";
+    } else {
+        self.playerLabel.text = @"X";
+    }
+}
 @end
